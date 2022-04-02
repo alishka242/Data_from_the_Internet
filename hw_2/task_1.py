540bde02b0861cdd68eeed327e756d276fadae8e
Alina Ch <89185720843@bk.ru>
2022-04-02 23:42:55 +0300

    checked something

--- hw_2/task_1.py
+++ hw_2/task_1.py
@@ -1,3 +1,4 @@
+from email import message
 from bs4 import BeautifulSoup as bs 
 import requests
 from pprint import pprint
@@ -34,7 +35,7 @@ def get_val_pages(dom):
     
     return val_pages
 
-def get_dict_vac(dom, base_url): 
+def get_dict_vac(dom, base_url, page): 
     """Получаем словарь с инфой о предложениях"""
     vacancies = dom.find_all('div', {'class':'vacancy-serp-item'})
     vacancies_data = {}
@@ -53,7 +54,7 @@ def get_dict_vac(dom, base_url):
         except:
             vac_salary = get_salary(False)
 
-        vacancies_data[f'v_{numb}'] = {'vac_name' : vac_name, 'vac_salary' : vac_salary, 'vac_link' : vac_link, 'vac_site' : vac_site}
+        vacancies_data[f'v_{numb}_p_{page}'] = {'vac_name' : vac_name, 'vac_salary' : vac_salary, 'vac_link' : vac_link, 'vac_site' : vac_site}
         
         numb +=1
     if vacancies_data:
@@ -68,6 +69,7 @@ def get_url_for_search_work(search_text, page = 0):
     if page == 0:
         search_page = f'{base_url}/search/vacancy?area=76&fromSearchLine=true&text={search_text}'
     else: 
+        page = page - 1
         search_page = f'{base_url}/vacancies/{search_text}?page={page}&hhtmFrom=vacancy_search_catalog'
 
     user_headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36'}
@@ -75,6 +77,7 @@ def get_url_for_search_work(search_text, page = 0):
     return [search_page, user_headers, base_url]
 
 if __name__ == '__main__':
+    err_message = 'Похоже Вы ошиблись при вводе, попробуйте еще раз'
     search_text = input('Введите желаемую должность: ').replace(' ', '+')  #'data+dev'
     url = get_url_for_search_work(search_text)
     response_site = requests.get(url[0], headers=url[1])
@@ -84,13 +87,16 @@ if __name__ == '__main__':
     vac_dict = {}
 
     if val_pages > 0:
-        user_page = input(f'По данному запросу есть {val_pages} стр, какую откроем? Введите число от 1 до {val_pages}: ')
-        url = get_url_for_search_work(search_text, user_page)
-        vac_dict = get_dict_vac(dom, url[2])
-    else: 
-        vac_dict = get_dict_vac(dom, url[2])
+        try:
+            user_page = int(input(f'По данному запросу есть {val_pages} стр, какую откроем? Введите число от 1 до {val_pages}: '))
+            url = get_url_for_search_work(search_text, user_page)
+        except:
+            pass
+    else:
+        user_page = 0
+        vac_dict = get_dict_vac(dom, url[2], user_page)
 
     if vac_dict:
         pprint(vac_dict)
     else:
-        print('Похоже Вы ошиблись при вводе, попробуйте еще раз')
+        print(err_message)
