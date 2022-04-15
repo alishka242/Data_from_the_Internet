2462a61e9bc3bc031db2db03dbb25793ebc37aba
Alina Churkina <89185720843@bk.ru>
2022-04-15 21:34:23 +0300

    I get list of the hrefs

--- hw_5/task_5_1.py
+++ hw_5/task_5_1.py
@@ -3,13 +3,18 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+from pprint import pprint
 
 user_email = 'study.ai_172@mail.ru'
 user_password = 'NextPassword172#'
 # s = Service('./chromedriver')
 driver = webdriver.Chrome()
 driver.get('https://mail.ru/?from=logout')
+
+#property of driver obj is wait load
+driver.implicitly_wait(15)
 
 site_name = driver.title
 print(site_name)
@@ -18,30 +23,64 @@ def find_elem(el_xpath):
     elem = driver.find_element(By.XPATH, el_xpath)
     return elem
 
-#log_in_
+#log_in
 find_elem("//div[contains(@class, 'logged-out-one-click')]/button").click()
 
 #get form
 driver.get(find_elem("//iframe[@class='ag-popup__frame__layout__iframe']").get_attribute('src'))
-time.sleep(.5)
 
 #send email
 find_elem("//div[@class='base-0-2-57']/div/div/input").send_keys(user_email)
 find_elem("//button[@class='base-0-2-79 primary-0-2-93']").send_keys(Keys.ENTER)
-time.sleep(.5)
 
 #send password
 find_elem("//input[contains(@class,'withIcon-0-2-72')]").send_keys(user_password)
 find_elem("//div[@class='submit-button-wrap']/*/button").send_keys(Keys.ENTER)
-time.sleep(.5)
 
-#STOP_POINT
-# list_mess = driver.find_element(By.XPATH, "//div[@class='ReactVirtualized__Grid__innerScrollContainer']/a").get_attribute('href') #.__getattribute__('href')
-# for href in list_mess:
-#     pass
-# time.sleep(.5)
-# print(list_mess)
+#Реализация, которая в будущем может работать быстрее, если получится разобраться, как отсортировать ненужные строки. Сейчас голова не варит -_-
+# list_mess = []
+# href_list = []
+# i = 0
+# # get href of messages
+# while i < 2:
+#     #get list of links
+#     list_mess = driver.find_elements(By.XPATH, "//div[@class='ReactVirtualized__Grid__innerScrollContainer']/a[contains(@class, 'js-tooltip-direction_letter-bottom')]")
+#     last_mess = list_mess[-1].get_attribute('href')
+#     if last_mess == list_mess[-1]:
+#         break
 
-driver.quit()
-# input = "//div[contains(@class,'ag-popup__frame__layout-desktop')]"
-# elem = driver.find_element(By.ID,)
+#     #insert href in href_list
+#     for href in list_mess:
+#         href_el = href.get_attribute('href')
+#         # pprint(href_el)
+#         # print(len(href_list))
+    
+#     #skrolling page
+#     actions = ActionChains(driver)
+#     actions.move_to_element(list_mess[-1]).perform()
+#     time.sleep(4)
+
+#     i +=1 
+
+#     # print(len(href_list))
+
+xpath = "//div[@class='ReactVirtualized__Grid__innerScrollContainer']/a[contains(@class, 'js-tooltip-direction_letter-bottom')]"
+
+links =[]
+last_id = None
+while True:  # собираем ссылки на новости, прокручивая список
+    letters = driver.find_elements(By.XPATH, xpath)  # сканируем все показанные
+    l = letters[-1].get_attribute('href')
+    if last_id == l:  # список показан полностью - заканчиваем сбор ссылок
+        break
+
+    for letter in letters:
+        links.append(letter.get_attribute('href').split('?')[0])
+
+    last_id = l
+    actions = ActionChains(driver)
+    actions.move_to_element(letters[-1]).perform()
+    time.sleep(4)
+
+links = list(set(links))  # удаляем дубликаты
+
--- hw_5/task_5_2.py
+++ hw_5/task_5_2.py
@@ -1,46 +1,99 @@
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-import time
+from selenium.webdriver.common.action_chains import ActionChains
+from pymongo import MongoClient
+from time import sleep
 
-# s = Service('./chromedriver')
+
+options = Options()
+options.add_argument("start-maximized")
+
 driver = webdriver.Chrome()
-driver.get('https://mail.ru/?from=logout')
+wait = WebDriverWait(driver, 15)
 
 
-def find_elem(el_xpath):
-    elem = driver.find_element_by_xpath(el_xpath)
-    return elem
+# === авторизация ===
+driver.get('https://account.mail.ru/login')
 
-user_email = 'study.ai_172@mail.ru'
-user_password = 'NextPassword172#'
+# логин
+elem = wait.until(EC.presence_of_element_located((By.NAME, 'username')))
+elem.send_keys("study.ai_172@mail.ru")
+elem.send_keys(Keys.ENTER)
 
-#log_in_
-elem = "//div[contains(@class, 'logged-out-one-click')]/button"
-find_elem(elem).click()
-
-#get form
-elem = find_elem("//iframe[@class='ag-popup__frame__layout__iframe']").get_attribute('src')
-driver.get(elem)
-time.sleep(.5)
-
-#find input-el and send email
-elem = find_elem("//div[@class='base-0-2-57']/div/div/input").send_keys(user_email)
-button = find_elem("//button[@class='base-0-2-79 primary-0-2-93']").send_keys(Keys.ENTER)
-time.sleep(.5)
-
-#find input-el and send password, and also log in
-elem = find_elem("//input[contains(@class,'withIcon-0-2-72')]").send_keys(user_password)
-button = find_elem("//div[@class='submit-button-wrap']/*/button").send_keys(Keys.ENTER)
+# пароль
+elem = wait.until(EC.presence_of_element_located((By.NAME, 'password')))
+sleep(1)  # не знаю как по-другому сделать
+elem.send_keys("NextPassword172#")
+elem.send_keys(Keys.ENTER)
 
 
-# list_mess = driver.find_elements_by_xpath("//div[@class='ReactVirtualized__Grid__innerScrollContainer']/a") #.__getattribute__('href')
-# time.sleep(.5)
-# print(type(list_mess))
+# === сбор ссылок на письма из общего списка ===
+links = []  # ссылки на страницы детального просмотра сообщений
+
+xpath = "//a[contains(@href,'/inbox/0:')]"  # строка с анонсом письма
+_ = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))  # ждём
+
+last_id = None
+while True:  # собираем ссылки на новости, прокручивая список
+    letters = driver.find_elements(By.XPATH, xpath)  # сканируем все показанные
+    l = letters[-1].get_attribute('href')
+    print(l)
+    if last_id == l:  # список показан полностью - заканчиваем сбор ссылок
+        break
+
+    for letter in letters:
+        links.append(letter.get_attribute('href').split('?')[0])
+
+    last_id = l
+    actions = ActionChains(driver)
+    actions.move_to_element(letters[-1]).perform()
+    sleep(4)
+
+links = list(set(links))  # удаляем дубликаты
 
 
+# === сканирование страниц детального просмотра, получение данных письем ===
+data = []  # список словарей с данными писем
 
-# input = "//div[contains(@class,'ag-popup__frame__layout-desktop')]"
-# elem = driver.find_element(By.ID,)
+for link in links:
+    driver.get(link)
+    # весь интерфейс грузится фоновыми запросами, поэтому сначала ожидаем
+    _ = wait.until(EC.presence_of_element_located((By.XPATH,"//h2[@class='thread-subject']")))
+
+    sleep(5)
+
+    h2 = wait.until(EC.presence_of_element_located((By.XPATH,"//h2[@class='thread-subject']"))).get_attribute("innerText")
+
+    frm = wait.until(EC.presence_of_element_located((By.XPATH,"//span[@class='letter-contact']"))).get_attribute("title")
+
+    date = wait.until(EC.presence_of_element_located((By.XPATH,"//div[@class='letter__date']"))).get_attribute("innerText")
+
+    text = wait.until(EC.presence_of_element_located((By.XPATH,"//div[@class='letter__body']"))).get_attribute("innerText")
+
+    el = {}
+    el['from'] = frm  # от кого,
+    el['date'] = date  # дата отправки,
+    el['topic'] = h2  # тема письма,
+    el['text'] = text.replace('\n', ' ').replace('\t', ' ')  # текст письма
+
+    data.append(el)
+
+
+# # === сохраняем результаты в базу ===
+# client = MongoClient('127.0.0.1', 27017)  # соединение
+# mongodb = client['letters']  # база
+# lettersdb = mongodb.lettersdb  # коллекция
+
+# for el in data:
+#     try:
+#         # el['_id'] = el['id']  # в будущем тут стоит поставить id письма
+#         lettersdb.insert_one(el)
+#     except Exception as e:
+#         print('Ошибка при сохранении новости в базу:')
+#         print(e)
